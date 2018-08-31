d1a3e240df409ea5976a690eab13cb02204c50dd
vhristov5555 <vasil.hristov@progress.com>
2018-08-31 18:22:17 +0300

    fix package json method to work with new structure of templates.

--- lib/project-helpers.ts
+++ lib/project-helpers.ts
@@ -1,11 +1,15 @@
 import { resolve } from "path";
-import { readFileSync } from "fs";
+import { readFileSync, existsSync } from "fs";
 
 import { PROJECT_DIR } from "./constants";
 
 export async function getInnerPackageJson() {
+    
     const appPath = resolve(PROJECT_DIR, "app");
-    return await getPackageJson(appPath);
+    const doExists = await existsSync(resolve(appPath, "package.json"))
+    const innerPackageJson = doExists ? await getPackageJson(appPath) : getPackageJson(resolve(PROJECT_DIR, "src"))
+    
+    return innerPackageJson;
 }
 
 export async function getPackageJson(projectDir = PROJECT_DIR) {
